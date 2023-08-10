a2afa0bba64e13aed7d315c7f62b6757c8dda8a4
obafisayo <abimbolaobafisayo@gmail.com>
2023-08-10 21:42:26 +0100

    updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -273,16 +273,16 @@ function Footer(){
                             <Totop
                                 component={
                                     <>
-                                        <Link className="footer__primary-link" to='/about-us' title='About Us' onMouseEnter={() => setHoverIt(true)} onMouseLeave={() => setHoverIt(false)}> 
+                                        <Link className="footer__primary-link" to="/about-us"  title='About Us' onMouseEnter={() => setHoverIt(true)} onMouseLeave={() => setHoverIt(false)}> 
                                             <PrimaryLinks link  ls={'0'} slide title={'About Us'} text={'ABOUT US'} mfsz={'3.4rem'} fsz={'5rem'} fw={'600'} height={'3px'} unslide hoverIt={hoverIt}/>
                                         </Link>
-                                        <Link className="footer__primary-link" to='/master-plan' title='Master-plan' onMouseEnter={() => setHoverIt1(true)} onMouseLeave={() => setHoverIt1(false)}>
+                                        <Link className="footer__primary-link" to="/master-plan" title='Master-plan' onMouseEnter={() => setHoverIt1(true)} onMouseLeave={() => setHoverIt1(false)}>
                                             <PrimaryLinks link  ls={'0'} slide title={'Master-plan'} text={'MASTER PLAN'} mfsz={'3.4rem'} fsz={'5rem'} fw={'600'} height={'3px'} unslide hoverIt={hoverIt1}/>
                                         </Link>
-                                        <Link className="footer__primary-link" to='/culture' title='Culture' onMouseEnter={() => setHoverIt2(true)} onMouseLeave={() => setHoverIt2(false)}>
+                                        <Link className="footer__primary-link" to="/culture" title='Culture' onMouseEnter={() => setHoverIt2(true)} onMouseLeave={() => setHoverIt2(false)}>
                                             <PrimaryLinks link ls={'0'} slide title={'Culture'} text={'CULTURE'} mfsz={'3.4rem'} fsz={'5rem'} fw={'600'} height={'3px'} unslide hoverIt={hoverIt2}/>
                                         </Link>
-                                        <Link className="footer__primary-link" to='/careers' title='Careers' onMouseEnter={() => setHoverIt3(true)} onMouseLeave={() => setHoverIt3(false)}>
+                                        <Link className="footer__primary-link" to="/careers" title='Careers' onMouseEnter={() => setHoverIt3(true)} onMouseLeave={() => setHoverIt3(false)}>
                                             <PrimaryLinks link ls={'0'} slide title={'Careers'} text={'CAREERS'} mfsz={'3.4rem'} fsz={'5rem'} fw={'600'} height={'3px'} unslide hoverIt={hoverIt3}/>
                                         </Link>
                                     </>
